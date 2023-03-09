361bab9f82b2b131ca826cceea5be3c4db51b756
DeveloperVikalp <agoodiosdeveloper@zohomail.in>
2023-03-09 16:50:50 +0530

    daytwo react assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+// npo
 import './App.css';
+import Compo from './compo';
 
 function App() {
+  const divStyle ={
+    backgroundColor: "green",
+    fontSize:"150px"
+    
+  }
+
+  const textHe = "compo text "
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={divStyle}>
+      <Compo  ourStyle ={divStyle}text={textHe}/>
+      <h2> heading two</h2>
     </div>
   );
 }
---
+++ src/compo.js
@@ -0,0 +1,9 @@
+function Compo(props){
+    return (
+        
+        <div style={props.ourStyle}>
+         <h1>{props.text}</h1>
+        </div>
+    )
+}
+export default Compo;
